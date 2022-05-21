c2dd12e8b8565623e8c86b053127a800695f20f4
Kyle Huskey <kylehuskey@gmail.com>
2022-05-21 15:48:49 -0400

    added all files

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const http = require('http')
 const express = require('express')
+const es6Renderer = require('express-es6-template-engine')
 
 const hostname = 'localhost'
 const port = 3000
 
 const app = express()
 const server = http.createServer(app)
+
+//template engine config
+app.engine('html', es6Renderer) //register es6Renderer as the html  template engine
+app.set('views', 'templates')  //set 'views' setting to look in 'templates' folder
+app.set('view engine', 'html') //set default 'view engine' to the one registered for html
+
+app.use(express.static('./public'))
+
+
+app.get('/', function(req, res){
+    res.render('home')
+})
+
+
+
+
+
+ 
+
+
+
+//tell server to start listening 
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+  })
