fb59519db179a539c5492a9f3512dfeac2d0fbc3
Kyle Huskey <kylehuskey@gmail.com>
2022-05-21 20:10:15 -0400

    mostly completed w/ adding routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const express = require('express')
 const es6Renderer = require('express-es6-template-engine')
+const ceos = require('./data')
+const data = require('./data')
 
 const hostname = 'localhost'
 const port = 3000
@@ -12,15 +14,44 @@ const server = http.createServer(app)
 app.engine('html', es6Renderer) //register es6Renderer as the html  template engine
 app.set('views', 'templates')  //set 'views' setting to look in 'templates' folder
 app.set('view engine', 'html') //set default 'view engine' to the one registered for html
-
+const partials = {
+    head: 'partials/head',
+    foot: 'partials/foot'
+}
+//public static
 app.use(express.static('./public'))
 
 
+//routes......
 app.get('/', function(req, res){
-    res.render('home')
+    res.render('home', {
+        partials,
+    })
 })
 
+app.get('/ceos', function(req, res){
+    res.render('ceo-list',{
+        partials,
+        locals: {
+            ceos
+        }
+    })
+})
 
+app.get('/ceos/:slug', function(req, res){
+    const renderedSlug = ceos.find(s => s.slug === req.params.slug)
+    console.log(renderedSlug)
+    if (!renderedSlug){
+        res.status(404).send(`Error, could not find a ceo with slug of: ${req.params.slug} `)
+        return
+    }
+    res.render('ceo-details', {
+        partials,
+        locals: {
+            renderedSlug
+        }
+    })
+})
 
 
 
